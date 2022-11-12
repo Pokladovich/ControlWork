e93c7b83ce86cc43109e46640b8c3e7b7b6000aa
sergey <bazaevserezha@gmail.com>
2022-11-12 11:11:51 +0300

    написана программа для контрольной 1 четверти

---
+++ ControlWork quarter/Program.cs
@@ -0,0 +1,25 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк длинна которых меньше либо равна 3.
+
+/*
+using System;
+using static System.Console;
+Clear();
+
+
+string[] m;
+WriteLine("Введите массив строк через пробел");
+string nameless = ReadLine();
+m = nameless.Split(' ');
+var result = new string[m .Length];
+var size = 0;
+foreach (var value in m )
+{
+    if (value.Length <= 3)
+    {
+    result[size] = value;
+    size++;
+    }
+}
+WriteLine(string.Join(" ", result, 0, size));
+*/
+
